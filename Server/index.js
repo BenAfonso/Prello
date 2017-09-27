7022f7024498a48e53f4bda81a497700799a56e5
BenAfonso <darkyler57@gmail.com>
2017-09-27 11:45:23 +0200

    Initial commit: server

---
+++ Server/index.js
@@ -0,0 +1,32 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const app = express()
+require('dotenv').config()
+
+app.use(bodyParser.json())
+
+app.all('/*', (req, res, next) => {
+  // CORS headers
+  res.header('Access-Control-Allow-Origin', '*')
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE')
+  // Custom headers
+  res.header('Access-Control-Allow-Headers', 'Content-Type,Accept,X-Access-Token')
+  if (req.method === 'OPTIONS') {
+    res.status(200).end()
+  } else {
+    next()
+  }
+})
+
+app.use('/', require('./routes'))
+
+app.use(function (req, res, next) {
+  res.status(404).send({
+    'status': 404,
+    'message': 'Not found !'
+  })
+})
+
+app.listen(process.env.PORT || 3000, () => {
+  console.log('Server listening at port ' + (process.env.PORT || 3000))
+})
