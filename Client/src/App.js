17eae8c41aae157b5a4462b352d0169741774055
BenAfonso <darkyler57@gmail.com>
2017-09-28 17:06:56 +0200

    client bootstrap

---
+++ Client/src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+import IndexPage from './pages/index.page'
+
+class App extends Component {
+  render () {
+    return (
+      <div className='App'>
+        Welcome on Prello
+
+        <IndexPage />
+      </div>
+    )
+  }
+}
+
+export default App
---
+++ Client/src/Components/Board/Board.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export default class Board extends React.Component {
+
+  constructor (props) {
+    super(props)
+  }
+
+  render () {
+    return (<div style={{
+    }}>
+      Board
+
+    </div>)
+  }
+}
