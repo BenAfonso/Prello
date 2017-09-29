5667e343404a8a897a76c01c3839b69fa25ccdad
BenAfonso <darkyler57@gmail.com>
2017-09-29 20:51:32 +0200

    adds basic react router

--- Client/src/App.js
+++ Client/src/App.js
@@ -1,12 +1,19 @@
 import React, { Component } from 'react'
 import IndexPage from './pages/index.page'
+import LoginPage from './pages/login.page'
+import RegisterPage from './pages/register.page'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
 
 class App extends Component {
   render () {
     return (
-      <div className='App'>
-        <IndexPage />
-      </div>
+      <Router>
+        <div className='App'>
+          <Route exact path='/' component={IndexPage} />
+          <Route path='/login' component={LoginPage} />
+          <Route path='/register' component={RegisterPage} />
+        </div>
+      </Router>
     )
   }
 }
---
+++ Client/src/pages/login.page.js
@@ -0,0 +1,5 @@
+import React from 'react'
+
+export default (props) => (
+  <h1>Login page</h1>
+)
