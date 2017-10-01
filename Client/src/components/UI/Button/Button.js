3d1a7ef0d4336d180612e12b78e8ff0e4498f347
BenAfonso <darkyler57@gmail.com>
2017-10-01 16:59:39 +0200

    feat<Button>: add button UI component and stories

--- Client/src/components/UI/Button/Button.js
+++ Client/src/components/UI/Button/Button.js
@@ -1,17 +1,107 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import cn from 'classnames'
+import styles from './Button.styles'
+import Color from 'color'
 
 export default class Button extends React.Component {
 
   static propTypes = {
+    height: PropTypes.string,
+    width: PropTypes.string,
+    color: PropTypes.string,
+    disabled: PropTypes.bool,
+    shadow: PropTypes.string,
+    round: PropTypes.bool,
+    gradient: PropTypes.string,
+    bgColor: PropTypes.string,
+    bgColorTo: PropTypes.string,
+    onClick: PropTypes.func,
+    children: PropTypes.any,
+    bold: PropTypes.bool,
   }
 
   static defaultProps = {
   }
 
+  constructor (props) {
+    super(props)
+    this.handleClick = this.handleClick.bind(this)
+  }
+
+  handleClick (event) {
+    const { disabled, onClick } = this.props
+
+    if (disabled) {
+      event.preventDefault()
+      event.stopPropagation()
+      return
+    }
+
+    if (onClick) {
+      onClick(event)
+    }
+  }
+
   render () {
+
+    const {
+      disabled,
+      children,
+      color,
+      round,
+      size,
+      width,
+      height,
+      bgColor,
+      bgColorTo,
+      borderRadius,
+      shadow,
+      bold,
+      gradient,
+      ...props
+    } = this.props
+
+    if (disabled) {
+      props.style = { pointerEvents: 'none', ...props.style }
+    }
+
+    const className = cn({
+      button: true,
+      disabled: disabled,
+      shadow: shadow,
+      round: round,
+      [size]: size
+    })
+
+    
+    const bgColorObj = Color(bgColor || '#55acf1')
+    const bgLuminosity = bgColorObj.luminosity()
+    const colorObj = Color(color || (bgLuminosity >= 0.5 ? 'black' : 'white'))
+
+    props.style = {
+      color: colorObj.string(), 
+      fontWeight: bold ? 'bold' : '',
+      backgroundColor: bgColorObj.string(),
+      borderRadius: round
+        ? '50%'
+        : borderRadius || '5px',
+      boxShadow: shadow
+        ? `0px 4px 0px ${bgColorObj.darken(0.2).string()}`
+        : 'none',
+      backgroundImage:
+        gradient || bgColorTo
+          ? `linear-gradient(to right, ${bgColor}, ${bgColorTo ||
+            bgColorObj.lighten(0.2).string()})` 
+          : 'none',
+      ...props.style
+    }
+
     return (
-      <div>Button</div>
+      <div {...props} className={className}>
+        {children}
+        <style jsx>{styles}</style>
+      </div>
     )
   }
 }
---
+++ Client/src/components/UI/Button/Button.stories.js
@@ -0,0 +1,51 @@
+import React from 'react'
+import { storiesOf } from '@storybook/react'
+import Button from './Button'
+import { withKnobs, text, color, select, boolean } from '@storybook/addon-knobs'
+
+storiesOf('Button', module)
+.addDecorator(withKnobs)
+.add('Plain button', () => (
+  <Button
+    bgColor={color('bgColor', '#5AAC44')}
+    color={color('color', '#fff')}
+    bold={boolean('bold', true)}
+  >
+    {text('Label', 'A simple button')}
+  </Button>
+))
+.add('With shadow and gradient', () => (
+  <Button
+    bgColor={color('bgColor', '#5AAC44')}
+    color={color('color', '#fff')}
+    shadow={boolean('shadow', true)}
+    gradient={boolean('gradient', true)}
+  >
+    {text('Label', 'With shadow & gradient')}
+  </Button>
+))
+.add('Custom gradient', () => (
+  <Button
+    bgColor={color('bgColor', '#81247A')}
+    bgColorTo={color('bgColorTo', '#303993')}
+    color={color('color', '#fff')}
+    shadow={boolean('shadow', true)}
+  >
+    {text('Label', 'Custom gradient')}
+  </Button>
+))
+.add('Custom dimensions', () => (
+  <Button width={text('Width', '300px')} height={text('height', '300px')}>
+    I can be any size and the text is centered and multiline
+  </Button>
+))
+.add('Disabled', () => (
+  <Button bgColor={color('bgColor', '#81247A')}
+    bgColorTo={color('bgColorTo', '#303993')}
+    color={color('color', '#fff')}
+    shadow={boolean('shadow', true)}
+    disabled={boolean('disabled', true)}
+  >
+    Disabled
+  </Button>
+))
