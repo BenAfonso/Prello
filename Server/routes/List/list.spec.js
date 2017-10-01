8e81255e51d34b6fb315e916a8690a9b6f0100a8
BenAfonso <darkyler57@gmail.com>
2017-10-01 20:26:20 +0200

    test<ListCRUD>: testing update list route

---
+++ Server/routes/List/list.spec.js
@@ -0,0 +1,48 @@
+const mongoose = require('mongoose')
+const Board = mongoose.model('Board')
+const List = mongoose.model('List')
+const mocha = require('mocha')
+const describe = mocha.describe
+const beforeEach = mocha.beforeEach
+const it = mocha.it
+const mockedBoard = { title: 'Test board', visibility: 'public', background: '' }
+const mockedList = { name: 'Test list' }
+
+module.exports = (server, chai) => {
+
+  chai.should()
+  let board = null
+  describe('Lists', () => {
+    beforeEach((done) => {
+      List.remove({where: {}}).then(() => {
+        Board.remove({where: {}}).then(() => {
+          Board.create(mockedBoard).then(b => {
+            board = b
+            done()
+          })
+        })
+      })
+    })
+
+    describe('UPDATE /boards/:boardId/lists/:listId', () => {
+      it('it should UPDATE a list', done => {
+        List.create(mockedList).then(list => {
+          chai.request(server)
+            .put(`/boards/${board._id}/lists/${list._id}`)
+            .send({name: 'Test list updated'})
+            .end((err, res) => {
+              res.should.have.status(200)
+              res.body.should.be.a('string')
+              List.findById(list._id, (err, res) => {
+                res.name.should.equal('Test list updated')
+                done()
+              })
+            })
+        }).catch(err => {
+          console.error('Error mocking test data')
+          return err
+        })
+      })
+    })
+  })
+}
