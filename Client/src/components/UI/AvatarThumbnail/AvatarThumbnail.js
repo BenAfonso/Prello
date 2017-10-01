c3d385274be55f86b4cf3c899332f0ec4030c4d6
BenAfonso <darkyler57@gmail.com>
2017-10-01 17:36:33 +0200

    feat<AvatarThumbnail>: UI Component and stories

---
+++ Client/src/components/UI/AvatarThumbnail/AvatarThumbnail.js
@@ -0,0 +1,68 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+export default class AvatarThumbnail extends React.Component {
+
+  static propTypes = {
+    thumbnail: PropTypes.string,
+    initials: PropTypes.string,
+    onClick: PropTypes.func,
+    bgColor: PropTypes.string,
+    size: PropTypes.string,
+    fontSize: PropTypes.string,
+    color: PropTypes.string
+  }
+
+  static defaultProps = {
+    color: '#FFF',
+    bgColor: '#444',
+    size: '50px',
+    fontSize: '30px'
+  }
+
+  render () {
+
+    const { 
+      thumbnail,
+      initials,
+      bgColor,
+      color,
+      fontSize,
+      size,
+      ...props } = this.props
+
+    props.style = {
+      color: color, 
+      backgroundColor: bgColor,
+      backgroundImage:
+        thumbnail
+          ? `url('${thumbnail}')`
+          : 'none',
+      fontSize: fontSize,
+      height: size,
+      width: size,
+      lineHeight: size,
+      ...props.style
+    }
+
+    return (
+      <div {...props} className='host'>
+        {
+          !thumbnail
+            ? initials
+            : ''
+        }
+        <style jsx>{`
+          .host {
+            background-position: center;
+            background-size: cover;
+            border-radius: 50%;
+            text-align: center;
+            font-family: 'Open Sans', sans-serif;
+            font-weight: bold;
+          }  
+        `}</style>
+      </div>
+    )
+  }
+}
