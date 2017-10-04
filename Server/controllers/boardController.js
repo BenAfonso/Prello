eaa9c186a7cf96deb4465f056322d050ab3f7b3f
zambran <zambrano.nicolas@outlook.com>
2017-10-04 19:43:39 +0200

    feat<List>: move list and update the board inside the database

--- Server/controllers/boardController.js
+++ Server/controllers/boardController.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
 const Board = mongoose.model('Board')
 const Card = mongoose.model('Card')
-
+const Util = require('./Util')
 const boardController = {}
 
 boardController.getAllBoards = function () {
@@ -54,6 +54,45 @@ boardController.removeListFromBoard = function (boardId, listId) {
         reject(err)
       } else {
         resolve(res)
+      }
+    })
+  })
+}
+boardController.moveList = function (req) {
+  return new Promise((resolve, reject) => {
+    if (req.params.boardId === null) {
+      reject(new Error('Missing boardID'))
+      return
+    }
+    let boardId = req.params.boardId
+    if (req.params.listId === null) {
+      reject(new Error('Missing listID'))
+      return
+    }
+    let listId = req.params.listId
+
+    if (req.body.position === null) {
+      reject(new Error('Missing Position'))
+      return
+    }
+    let position = req.body.position
+    Board.findOne({ '_id': boardId }).exec(function (err, res) {
+      if (err) {
+        reject(err)
+      } else {
+        let indexList = res.lists.indexOf(listId)
+        if (indexList === -1) {
+          reject(new Error('List ID not found in the board'))
+          return
+        }
+        let newLists = Util.moveInsideAnArray(res.lists, indexList, position)
+        Board.findOneAndUpdate({'_id': boardId}, {'lists': newLists}, {new: true}, function (err, res) {
+          if (err) {
+            reject(err)
+          } else {
+            resolve(res)
+          }
+        })
       }
     })
   })
