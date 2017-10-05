44ada14c8e96c91d7bc7cd53a33c5a105c701ed5
BenAfonso <darkyler57@gmail.com>
2017-10-05 20:41:31 +0200

    feat<Sockets>: redis distributed cache

--- Server/controllers/sockets.js
+++ Server/controllers/sockets.js
@@ -1,13 +1,62 @@
 const io = require('socket.io')()
+const redis = require('socket.io-redis')
+
+io.adapter(redis({ host: 'localhost', port: 6379 }))
 
 io.on('connection', (client) => {
   // here you can start emitting events to the client
-  console.log('new client')
+  log('New connection')
   client.on('subscribeToBoard', (boardId) => {
-    console.log('client is subscribing to board', boardId)
+    subscribeToBoard(client, boardId)
+  })
+
+  client.on('disconnect', client => {
+    log('Client disconnected')
+    leaveBoards(client)
   })
 })
+
+function subscribeToBoard (client, boardId) {
+  io.of('/').adapter.remoteJoin(client.id, boardId, (err) => {
+    if (err) { return console.error(err) }
+    log('User subscribed to REDIS')
+    getUsersInBoard(boardId).then(users => {
+      log(`REDIS USERS ON BOARD ${boardId}: ${users}`)
+    }).catch(err => {
+      return console.error(err)
+    })
+    // success
+  })
+}
+
+function unsubscribeToBoard (client, boardId) {
+  io.of('/').adapter.removeLeave(client.id, boardId, err => {
+    if (err) { return console.error(err) }
+    // Process
+  })
+}
+
+function leaveBoards (client) {
+  io.of('/').adapter.remoteDisconnect(client.id, true, (err) => {
+    if (err) { return console.error(err) }
+    log(`REDIS CLIENT LEFT`)
+    // success
+  })
+}
+
+function getUsersInBoard (boardId) {
+  return new Promise((resolve, reject) => {
+    io.of('/').adapter.clients(boardId, (err, clients) => {
+      if (err) { return reject(new Error(`Error getting users in board ${boardId}`)) }
+      return resolve(clients)
+    })
+  })
+}
 
 const port = 8000
 io.listen(port)
 console.log('Socket.io listening on port ', port)
+
+function log (message) {
+  console.log(`[SOCKETS]: ${message}`)
+}
