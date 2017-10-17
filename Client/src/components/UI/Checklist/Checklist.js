ba69394728c3a0f444f305b81349391d04e9c233
maxcabourg <max.cabourg@etu.umontpellier.fr>
2017-10-17 21:47:39 +0200

    <UI/Checklist> Add item, edit title

--- Client/src/components/UI/Checklist/Checklist.js
+++ Client/src/components/UI/Checklist/Checklist.js
@@ -1,27 +1,67 @@
 import React from 'react'
 import ChecklistItem from './ChecklistItem'
+import PropTypes from 'prop-types'
 import styles from './Checklist.styles'
 import Input from '../Input/Input'
 import Button from '../Button/Button'
 
 export default class Checklist extends React.Component {
+
+  static propTypes = {
+    items: PropTypes.array,
+    title: PropTypes.string.isRequired
+  }
+
+  static defaultProps = {
+    items: []
+  }
+
   constructor (props) {
     super(props)
     this.state = {
-      items: this.props.items,
-      displayNewItemForm: false
+      items: props.items,
+      displayNewItemForm: false,
+      displayEditTitleForm: false,
+      title: props.title
     }
     this.displayNewItemForm = this.displayNewItemForm.bind(this)
     this.addItem = this.addItem.bind(this)
     this.hideNewItemForm = this.hideNewItemForm.bind(this)
+    this.displayEditTitleForm = this.displayEditTitleForm.bind(this)
+    this.hideEditTitleForm = this.hideEditTitleForm.bind(this)
+    this.updateTitle = this.updateTitle.bind(this)
   }
 
   displayNewItemForm () {
     this.setState({displayNewItemForm: true})
   }
 
-  addItem (text) {
-    this.setState({items: this.state.items.slice().push(this.newItemInput.value)})
+  displayEditTitleForm () {
+    this.setState({displayEditTitleForm: true})
+  }
+
+  hideEditTitleForm() {
+    this.setState({displayEditTitleForm: false})
+  }
+
+  updateTitle() {
+    this.setState({title: this.titleInput.input.value, displayEditTitleForm: false})
+  }
+
+  addItem () {
+    if(this.textInput.input.value.length > 0) {
+      let newItemsList = this.state.items.slice()
+      newItemsList.push({content: this.textInput.input.value, done: false})
+      this.setState({items: newItemsList})
+      this.textInput.input.value = ''
+    }
+  }
+
+  deleteItem (index) {
+    console.log('index '+index)
+    let newItemsList = this.state.items.slice()
+    newItemsList.splice(index, 1)
+    this.setState({items: newItemsList})
   }
 
   hideNewItemForm () {
@@ -31,16 +71,25 @@ export default class Checklist extends React.Component {
   render () {
     return (
       <div className='Checklist'>
+        {!this.state.displayEditTitleForm ? 
+        <h2 onClick={this.displayEditTitleForm}>{this.state.title}</h2>
+        :
+        <div className='editTitleForm'>
+          <Input ref={(v) => this.titleInput = v} placeholder={this.state.title} />
+          <Button onClick={this.updateTitle} bgColor='#3cb221' color='#FFF'>Update title</Button>
+          <Button onClick={this.hideEditTitleForm} bgColor='#F00' color='#FFF'>X</Button>
+        </div> }
         <div className='progressBar' />
         <div className='actualProgressBar' />
-        {this.state.items.map(item => (<ChecklistItem content={item.content} />))}
+        {this.state.items.map((item, key) => (<div>
+          <ChecklistItem key={key} content={item.content} /> <Button onClick={(e) => this.deleteItem(key)} color='#FFF' bgColor='#F00'>X</Button>
+          </div>))}
         {!this.state.displayNewItemForm ? <div>
-          <input type='button' onClick={this.displayNewItemForm} value='Add an item' />
+          <Button onClick={this.displayNewItemForm} >Add an item</Button>
         </div> : <div className='addItemDiv'>
-          <input type='text' ref={(t) => { this.newItemInput = t }} placeholder='Describe your item...' />
-          <Input placeholder='Describe your item...' />
-          <input type='button' onClick={this.addItem} value='Confirm' />
-          <input type='button' onClick={this.hideNewItemForm} value='Cancel' />
+          <Input ref={(v) => this.textInput = v} placeholder='Describe your item...' />
+          <Button onClick={this.addItem} >Confirm</Button>
+          <Button onClick={this.hideNewItemForm} >Cancel</Button>
         </div>}
         <style jsx>{styles}</style>
       </div>
