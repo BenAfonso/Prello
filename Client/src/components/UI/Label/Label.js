0384db3a5d175e079aa13e9d65711e2dc88da78b
Mukary <perso.rahim@gmail.com>
2017-10-23 10:23:34 +0200

    fix<Label>: standard format and render method

--- Client/src/components/UI/Label/Label.js
+++ Client/src/components/UI/Label/Label.js
@@ -2,84 +2,70 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import cn from 'classnames'
 
-export default class Label extends React.Component{
-    constructor(){
-        super()
-        this.state={
-            isExpanded: false,
-            labelTitle: '',
-        }
-        this.expandLabel = this.expandLabel.bind(this)
+export default class Label extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      isExpanded: false,
+      labelTitle: '',
     }
-    static propTypes = {
-        labelText: PropTypes.string,
-        width: PropTypes.string,
-        color: PropTypes.string,
-        style: PropTypes.object,
-        color: PropTypes.string,
-        backgroundColor: PropTypes.string,
-        fontWeight: PropTypes.string
-      }
+    this.expandLabel = this.expandLabel.bind(this)
+  }
+  static propTypes = {
+    labelText: PropTypes.string,
+    width: PropTypes.string,
+    color: PropTypes.string,
+    style: PropTypes.object,
+    color: PropTypes.string,
+    backgroundColor: PropTypes.string,
+    fontWeight: PropTypes.string
+  }
 
 
 
-      static defaultProps = {
-        height: '1pc',
-        width: '150px',
-        color: '#fff',
-        backgroundColor: '#000',
-        fontWeight: "bold",
-        borderRadius: "3px",
-        fontSize: '12px',
-        centeredText: true
-      }
+  static defaultProps = {
+    height: '1pc',
+    width: '150px',
+    color: '#fff',
+    backgroundColor: '#000',
+    fontWeight: "bold",
+    borderRadius: "3px",
+    fontSize: '12px',
+    centeredText: true
+  }
 
-      expandLabel(){
-          this.setState({
-              isExpanded: !this.state.isExpanded
-          }, () =>{
-              if(this.state.isExpanded) {
-                  this.setState({
-                    labelTitle: this.props.labelText
-                    
-                  })
-              }
-              else{
-                this.setState({
-                    labelTitle: ''
-                })
-              }
-          })
-      }
+  expandLabel() {
+    this.setState({
+      isExpanded: !this.state.isExpanded
+    })
+  }
 
-      render(){
-        const {
+  render() {
+    const {
             labelText,
-            width,
-            height,
-            fontWeight,
-            backgroundColor,
-            borderRadius,
-            color,
-            fontSize,
-            centeredText,
-            ...props
+      width,
+      height,
+      fontWeight,
+      backgroundColor,
+      borderRadius,
+      color,
+      fontSize,
+      centeredText,
+      ...props
         } = this.props
 
-        props.style={
-            width,
-            height: this.state.isExpanded ? '1pc' : '8px' ,
-            fontSize,
-            fontWeight,
-            backgroundColor,
-            borderRadius,
-            color,
-            textAlign: centeredText ? 'center' : 'left',
-            ...props.style
-        }
-
-        return (
-            <div style={props.style} onClick={this.expandLabel}>{this.state.labelTitle}</div>
-        )
-      }
+    props.style = {
+      width,
+      height: this.state.isExpanded ? '1pc' : '8px',
+      fontSize,
+      fontWeight,
+      backgroundColor,
+      borderRadius,
+      color,
+      textAlign: centeredText ? 'center' : 'left',
+      ...props.style
+    }
+    if (this.state.isExpanded) return <div style={props.style} onClick={this.expandLabel}>{this.props.labelText}</div>
+    else return <div style={props.style} onClick={this.expandLabel}></div>
+  }
 }
--- Client/src/components/UI/Label/Label.stories.js
+++ Client/src/components/UI/Label/Label.stories.js
@@ -1,15 +1,10 @@
 import React from 'react'
 import { storiesOf } from '@storybook/react'
 import Label from './Label'
-import { withKnobs, text, boolean } from '@storybook/addon-knobs'
-
-
 
 storiesOf('Label', module)
-.addDecorator(withKnobs)
 .add('Front-end', () => (
-  <Label 
-    labelText="Front-end"
-    backgroundColor="#61BD4F"
+  <Label labelText='Front-end'
+    backgroundColor='#61BD4F'
   />
 ))
