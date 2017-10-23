18c2d0a1855805fe6952ae757de0a04303d2341d
maxcabourg <max.cabourg@etu.umontpellier.fr>
2017-10-23 15:33:41 +0200

    <Checklist> add test

--- Client/src/store/reducer.js
+++ Client/src/store/reducer.js
@@ -144,25 +144,28 @@ export default function reducer (state = defaultState, action) {
       }
     }
     case 'ADD_CHECKLIST': {
-      let newLists = state.board.lists.map((list) => {
-        list.cards.map((card) => {
-          if (card._id === action.payload.cardId) {
-            card.checklists.push({title: action.payload.title, index: card.checklists.length, items: []})
-          }
-          return card
+      if (action.payload.title.length > 0) {
+        let newLists = state.board.lists.map((list) => {
+          list.cards.map((card) => {
+            if (card._id === action.payload.cardId) {
+              card.checklists.push({title: action.payload.title, index: card.checklists.length, items: []})
+            }
+            return card
+          })
+          return list
         })
-        return list
-      })
-      return {
-        ...state,
-        board: {
-          ...state.board,
-          lists: newLists
+        return {
+          ...state,
+          board: {
+            ...state.board,
+            lists: newLists
+          }
         }
+      } else {
+        return state
       }
     }
     case 'DELETE_CHECKLIST': {
-      console.log('reducer '+action.payload.cardId+' '+action.payload.checklistIndex)
       let newLists = state.board.lists.map((list) => {
         list.cards.map((card) => {
           if (card._id === action.payload.cardId) {
