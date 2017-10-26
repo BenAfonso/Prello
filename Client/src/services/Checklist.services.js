2cf6405bc09936a1d6c475dba61b064c9bdaaacf
zambran <zambrano.nicolas@outlook.com>
2017-10-26 15:15:29 +0200

    fix<CreateCard>:Removing .then and .catch when call API

--- Client/src/services/Checklist.services.js
+++ Client/src/services/Checklist.services.js
@@ -2,39 +2,17 @@ import axios from 'axios'
 import Config from '../config'
 
 export function addChecklist (boardId, listId, cardId, title) {
-  return new Promise((resolve, reject) => {
-    axios.post(`${Config.API_URL}/boards/${boardId}/lists/${listId}/cards/${cardId}/checklists`, {
-      text: title
-    })
-      .then(res => {
-        resolve(res.data)
-      })
-      .catch(err => {
-        reject(err)
-      })
+  axios.post(`${Config.API_URL}/boards/${boardId}/lists/${listId}/cards/${cardId}/checklists`, {
+    text: title
   })
 }
 
 export function deleteChecklist (boardId, listId, cardId, checklistId) {
-  return new Promise((resolve, reject) => {
-    axios.delete(`${Config.API_URL}/boards/${boardId}/cards/${cardId}/checklists/${checklistId}`)
-      .then(res => {
-        resolve(res.data)
-      }).catch(err => {
-        reject(err)
-      })
-  })
+  axios.delete(`${Config.API_URL}/boards/${boardId}/cards/${cardId}/checklists/${checklistId}`)
 }
 
 export function updateChecklist (boardId, listId, cardId, checklistId, newTitle) {
-  return new Promise((resolve, reject) => {
-    axios.put(`${Config.API_URL}/boards/${boardId}/cards/${cardId}/checklists/${checklistId}`, {
-      text: newTitle
-    })
-      .then(res => {
-        resolve(res.data)
-      }).catch(err => {
-        resolve(err)
-      })
+  axios.put(`${Config.API_URL}/boards/${boardId}/cards/${cardId}/checklists/${checklistId}`, {
+    text: newTitle
   })
 }
