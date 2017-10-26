196e1bd6295d4e4577e057b5d8b3e6c5b1b04925
BenAfonso <darkyler57@gmail.com>
2017-10-26 15:29:16 +0200

    feat<ArchiveUI>: archived section on side menu

---
+++ Client/src/components/MenuElements/ArchivedMenu/ArchivedMenu.js
@@ -0,0 +1,89 @@
+import React from 'react'
+import {connect} from 'react-redux'
+import Card from '../../Card/Card'
+import { restoreCard, restoreList } from '../../../store/actions'
+
+@connect(store => {
+  return {
+    board: store.board
+  }
+})
+export default class ArchivedMenu extends React.Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      cardTab: false
+    }
+  }
+
+  switchTab () {
+    this.setState({ cardTab: !this.state.cardTab })
+  }
+  render () {
+    return (
+      <div className='host'>
+        <div className='title'>
+          ARCHIVED { this.state.cardTab ? 'CARDS' : 'LISTS' }
+          <span onClick={this.switchTab.bind(this)}>Switch to { this.state.cardTab ? 'lists' : 'cards' }</span>
+        </div>
+        <ul>
+          {
+            this.props.board.lists.map(l => (
+              this.state.cardTab
+                ? l.cards.map(c => c.isArchived
+                  ? <li>
+                    <Card
+                      content={c.text}
+                      createdAt={c.createdAt}
+                      index={0}
+                      listIndex={0}
+                      collaborators={c.collaborators}
+                      id={c._id} />
+                    <div className='restoreButton' onClick={() => restoreCard(this.props.board._id, l._id, c)}>Restore</div>
+                  </li>
+                  : null
+                )
+                : l.isArchived
+                  ? <li>
+                    <Card
+                      content={l.name}
+                      createdAt={l.createdAt}
+                      index={0}
+                      listIndex={0}
+                      collaborators={[]}
+                      id={l._id} />
+                    <div className='restoreButton' onClick={() => restoreList(this.props.board._id, l)}>Restore</div>
+                  </li>
+                  : null
+            ))
+          }
+        </ul>
+        <style jsx>{`
+          li {
+            margin-bottom: 10px;
+          }
+
+          .title {
+            font-weight: bold;
+            margin-bottom: 20px;
+          }
+
+          .title span {
+            display: block;
+            font-size: 14px;
+            font-weight: regular;
+            cursor: pointer;
+            color: #999;
+          }
+
+          .restoreButton {
+            cursor: pointer;
+            font-size: 14px;
+            color: #999;
+          }
+        `}
+        </style>
+      </div>
+    )
+  }
+}
