4071442fd5d41bb114f4a4ba4c38cffbc5f636ab
zambran <zambrano.nicolas@outlook.com>
2017-10-26 14:43:05 +0200

    refactoring<ServerModel>: Adding createdAt attribute

--- Server/models/Attachment.js
+++ Server/models/Attachment.js
@@ -5,6 +5,7 @@ const Schema = mongoose.Schema
 const AttachmentSchema = new Schema({
   name: {type: String, default: ''},
   url: {type: String}
+  createdAt: {type: Date, default: Date.now}  
 })
 
 mongoose.model('Attachment', AttachmentSchema)
