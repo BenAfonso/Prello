567938ab14939d79422a1a70ede2f05a97f72a85
Mukary <perso.rahim@gmail.com>
2017-11-09 22:37:21 +0100

    add<Charts>: preparing analytics calls to API with correct REST format

--- Client/src/services/Charts.services.js
+++ Client/src/services/Charts.services.js
@@ -7,8 +7,20 @@ export function getMembersAnalytics (boardId) {
   )
 }
 
-export function getOwnersAnalytics (boardId) {
-  axios.get(`${Config.API_URL}/boards/${boardId}/analytics/owners`).then(data =>
+export function getResponsibleAnalytics (boardId) {
+  axios.get(`${Config.API_URL}/boards/${boardId}/analytics/responsibles`).then(data =>
+    console.log(data)
+  )
+}
+
+export function getListsAnalytics (boardId) {
+  axios.get(`${Config.API_URL}/boards/${boardId}/analytics/lists`).then(data =>
+    console.log(data)
+  )
+}
+
+export function getOverviewAnalytics (boardId) {
+  axios.get(`${Config.API_URL}/boards/${boardId}/analytics/overview`).then(data =>
     console.log(data)
   )
 }
