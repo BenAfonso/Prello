39ef01dd734a6f1e8df66f6604ee928a6a9a9f26
Maroane <maroane.hamida@gmail.com>
2017-11-16 18:11:20 +0100

    feat<search_bar>: search user's boards and teams + search users + dropdown style

--- Client/src/components/SearchBar/SearchBar.js
+++ Client/src/components/SearchBar/SearchBar.js
@@ -1,7 +1,11 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 import styles from './SearchBar.styles'
 import {connect} from 'react-redux'
 import DropDown from '../UI/DropDown/DropDown'
+import AvatarThumbnail from '../UI/AvatarThumbnail/AvatarThumbnail'
+import { setBoardslist, setTeamslist, fetchMatchingUsers } from '../../store/actions'
+import { subscribeToBoardslist } from '../../services/api'
 
 @connect(store => {
   return {
@@ -20,7 +24,103 @@ export default class SearchBar extends React.Component {
   constructor (props) {
     super(props)
     this.state = {
-      barWidth: '200px'
+      matchingBoards: [],
+      matchingTeams: [],
+      matchingCards: [],
+      matchingUsers: []
+    }
+    this.onChange = this.onChange.bind(this)
+    this.setMatchingBoards = this.setMatchingBoards.bind(this)
+    this.setMatchingCards = this.setMatchingCards.bind(this)
+    this.setMatchingUsers = this.setMatchingUsers.bind(this)
+    this.setMatchingTeams = this.setMatchingTeams.bind(this)
+    this.renderUser = this.renderUser.bind(this)
+  }
+
+  componentDidMount () {
+    setBoardslist(this.props.dispatch).then(() => {
+      subscribeToBoardslist('testID')
+    }).catch(err => {
+      console.error(err)
+    })
+    setTeamslist(this.props.dispatch).then(() => {
+    }).catch(err => {
+      console.error(err)
+    })
+  }
+
+  setMatchingBoards (input) {
+    const reg = new RegExp(input, 'i')
+    let newMatchingBoards = []
+    this.props.boardslist.boards.map(board => board.title.match(reg) ? newMatchingBoards.push(board) : null)
+    this.setState({matchingBoards: newMatchingBoards.slice(0, 10)})
+  }
+
+  setMatchingCards (input) {
+  }
+
+  setMatchingUsers (input) {
+    let newMatchingUsers = []
+    fetchMatchingUsers(input).then(users => {
+      users.map(user =>
+        newMatchingUsers.push(user)
+      )
+      this.setState({matchingUsers: newMatchingUsers.slice(0, 10)})
+    })
+  }
+
+  setMatchingTeams (input) {
+    const reg = new RegExp(input, 'i')
+    let newMatchingTeams = []
+    this.props.teamslist.teams.map(team => team.name.match(reg) ? newMatchingTeams.push(team) : null)
+    this.setState({matchingTeams: newMatchingTeams.slice(0, 10)})
+  }
+
+  renderBoard (board) {
+  }
+
+  renderCard (card) {
+  }
+
+  renderTeam (team) {
+  }
+
+  renderUser (user) {
+    return (
+      <div className='user'>
+        <div className='user-thumbnail'>
+          <AvatarThumbnail
+            size='30px'
+            fontSize=''
+            thumbnail={user.picture}
+            initials={this.getInitials(user.name)}
+            bgColor={user.bgColor}
+            color='black'
+          />
+        </div>
+        <div className='user-infos'>
+          <div className='user-name'>{user.name}</div>
+          <div className='user-email'>{user.email}</div>
+        </div>
+        <style jsx>{styles}</style>
+      </div>
+    )
+  }
+
+  onChange () {
+    const input = this.input.value
+    if (input !== '') {
+      this.setMatchingBoards(input)
+      this.setMatchingCards(input)
+      this.setMatchingUsers(input)
+      this.setMatchingTeams(input)
+    } else {
+      this.setState({
+        matchingBoards: [],
+        matchingCards: [],
+        matchingTeams: [],
+        matchingUsers: []
+      })
     }
   }
 
@@ -28,25 +128,98 @@ export default class SearchBar extends React.Component {
     event.target.select()
   }
 
-  render () {
-    let menuElements = []
+  getInitials (name) {
+    const matches = name.match(/\b(\w)/g)
+    const initials = matches.join('').toUpperCase()
+    return initials
+  }
 
+  render () {
+    const { matchingBoards, matchingCards, matchingTeams, matchingUsers } = this.state
     return (
       <div className='host'>
         <DropDown
           layout='custom'
           orientation='left'
-          button={<input type='text' className='search-input' style={{ width: this.state.barWidth }} onFocus={this.handleFocus} />}
-          title='Search'>
-          <div style={{ width: '300px' }}>
-            <ul>
-              {
-                menuElements.length > 0
-                  ? null
-                  : <li className='element'>
-                    <div className='element-text'>Nothing! Yaaay</div>
-                  </li>
-              }
+          button={<input type='text' className='search-input' ref={(input) => { this.input = input }} onFocus={this.handleFocus} onChange={this.onChange} />}
+        >
+          <div className='search-content'>
+            <ul className='search-column'>
+              <li className='section'>
+                <div className='section-title'>Boards</div>
+                <ul className='section-elements'>
+                  {
+                    matchingBoards.length === 0
+                      ? <li className='element'>
+                        <div className='element-text'>Nothing! Yaaay</div>
+                      </li>
+                      : matchingBoards.map(board =>
+                        <Link to={`/boards/${board._id}`}>
+                          <li className='element' key={board._id}>
+                            <div className='content'>
+                              <div className='element-title'>{board.title}</div>
+                            </div>
+                            <div className='separator' />
+                          </li>
+                        </Link>
+                      )
+                  }
+                </ul>
+              </li>
+              <li className='section'>
+                <div className='section-title'>Cards</div>
+                <ul className='section-elements'>
+                  {
+                    matchingCards.length === 0
+                      ? <li className='element'>
+                        <div className='element-text'>Nothing! Yaaay</div>
+                      </li>
+                      : matchingCards.map(card =>
+                        <li className='element'>{this.renderCard(card)}</li>
+                      )
+                  }
+                </ul>
+              </li>
+            </ul>
+            <ul className='search-column'>
+              <li className='section'>
+                <div className='section-title'>Teams</div>
+                <ul className='section-elements'>
+                  {
+                    matchingTeams.length === 0
+                      ? <li className='element'>
+                        <div className='element-text'>Nothing! Yaaay</div>
+                      </li>
+                      : matchingTeams.map(team =>
+                        <Link to={`/teams/${team._id}`}>
+                          <li className='element'>
+                            <div className='content'>
+                              <div className='element-title'>{team.name}</div>
+                              <div className='element-text'>{team.users.length} {team.users.length > 1 ? 'members' : 'member'}</div>
+                            </div>
+                            <div className='separator' />
+                          </li>
+                        </Link>
+                      )
+                  }
+                </ul>
+              </li>
+              <li className='section'>
+                <div className='section-title'>Users</div>
+                <ul className='section-elements'>
+                  {
+                    matchingUsers.length === 0
+                      ? <li className='element'>
+                        <div className='element-text'>Nothing! Yaaay</div>
+                      </li>
+                      : matchingUsers.map(user =>
+                        <Link to={`/users/${user._id}/profile`}>
+                          <li className='element'>{this.renderUser(user)}</li>
+                        </Link>
+                      )
+                  }
+                </ul>
+              </li>
             </ul>
           </div>
         </DropDown>
--- Client/src/components/SearchBar/SearchBar.styles.js
+++ Client/src/components/SearchBar/SearchBar.styles.js
@@ -1,6 +1,6 @@
 export default `
 .host {
-
+  text-align: left;
 }
 
 input {
@@ -18,14 +18,87 @@ input:focus {
   min-width: 300px;
 }
 
+.search-content {
+  width: 600px;
+}
+
+.separator {
+  content: '';
+  height: 1px;
+  padding: 0;
+  background-color: #aaa;
+}
+
+.search-column {
+  display: flex;
+}
+
+.section {
+  width: 300px;
+  padding: 10px 0;
+}
+
+.section-title {
+  font-size: 18px;
+  font-weight: bold;
+  text-align: center;
+  padding-bottom: 10px;
+}
+
+.section-elements {
+  overflow-y: auto; 
+  max-height: 200px;
+}
+
 .element {
-  padding: 15px;
+  padding: 0 10px;
+}
+
+.element:hover {
+  background: #ff3399;
+  color: white;
+}
+
+.element:hover .user-name, .element:hover .user-email {
+  color: white;
+}
+
+.content {
+  width: 100%;
+  padding: 5px 0;  
+}
+
+.element-title {
+  padding: 4px 0px;
+  font-size: 15px;
+  font-weight: bold;
 }
 
 .element-text {
   padding: 4px 0px;
-  font-size: 15px;
-  text-align: left;
+  font-size: 13px;
+}
+
+.user {
+  padding: 10px 0;
+  display: flex;
+  align-items: center;
+}
+
+.user-infos {
+  padding: 0 10px;
+  overflow: hidden;
+}
+
+.user-name {
+  font-weight: bold;
+  color: #000;
+}
+
+.user-email {
+  font-style: italic;
+  font-size: 13px;
+  color: #999;        
 }
 
 `
