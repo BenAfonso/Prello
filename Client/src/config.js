1e24803e58e8fcd157e30e8016f16bd6dddd6d9d
zambran <zambrano.nicolas@outlook.com>
2017-11-23 21:26:41 +0100

    fix port

--- Client/src/config.js
+++ Client/src/config.js
@@ -1,6 +1,6 @@
 const Config = {
-  API_URL: process.env.REACT_APP_API_URL || 'http://localhost:3333',
-  SOCKET_URL: process.env.REACT_APP_SOCKET_URL || 'http://localhost:8000'
+  API_URL: process.env.REACT_APP_API_URL || 'http://176.142.249.180:3333',
+  SOCKET_URL: process.env.REACT_APP_SOCKET_URL || 'http://176.142.249.180:8000'
 }
 
 export default Config
